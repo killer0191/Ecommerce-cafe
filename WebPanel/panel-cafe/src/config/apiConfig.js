10bcc7db8b8763c2f21aa02745fb94a9772b4430
Adry Moises <adrymoises.arias.morales@gmail.com>
2024-10-27 12:18:13 -0600

    aux

--- WebPanel/panel-cafe/src/config/apiConfig.js
+++ WebPanel/panel-cafe/src/config/apiConfig.js
@@ -1,3 +1,3 @@
 // src/config/apiConfig.js
-export const API_BASE_URL = 'http://apiwebcafe.somee.com';
+export const API_BASE_URL = 'https://apiwebcafe.somee.com';
 //export const API_BASE_URL = 'https://localhost:7075';
